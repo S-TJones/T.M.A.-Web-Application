ca8f352ea8a76abf6414fcb0d4a4607ad9a5e436
S-TJones <jones.shemar.sj@gmail.com>
2020-12-05 12:40:31 -0500

    Added the Connection URL
    My connection URL is : postgres://tjftrjrivugxxj:4317e7d1952b845ffb995bb61b2cc86c6056ddfd655e7586ed548c7b032a4010@ec2-54-85-13-135.compute-1.amazonaws.com:5432/daqc95jtc3j6st . That's what I added.

--- Flask-2/app/__init__.py
+++ Flask-2/app/__init__.py
@@ -7,11 +7,14 @@ from flask_wtf.csrf import CSRFProtect
 
 from flask import render_template
 app = Flask(__name__)
-SQLALCHEMY_DATABASE_URI = "postgres://pyogevjdhhpdbe:dd8dca38e3a5ccb55e7e4eb4f1b0d2b1cb29a624ef71e182552480b86bdfdd6f@ec2-52-87-135-240.compute-1.amazonaws.com:5432/d7gshjrv8bbr12"
+
+SQLALCHEMY_DATABASE_URI = "postgres://tjftrjrivugxxj:4317e7d1952b845ffb995bb61b2cc86c6056ddfd655e7586ed548c7b032a4010@ec2-54-85-13-135.compute-1.amazonaws.com:5432/daqc95jtc3j6st"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = "./app/static/uploads"
 SECRET_KEY = 'Sup3r$3cretkey'
+
 app.config.from_object(__name__)
+
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 login = LoginManager(app) 
