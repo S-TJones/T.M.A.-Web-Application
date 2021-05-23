900b102ae380a38072389daf99339cfeed87f7f9
Shemar Jones <jones.shemar.sj@gmail.com>
2021-05-23 13:55:11 -0500

    Got the Tasks showing
    Turns out, it was the CSS all along. But it shows now.

--- app/static/scripts/dash.js
+++ app/static/scripts/dash.js
@@ -17,3 +17,29 @@ function closeNav() {
 }
 
 // Other things
+function getDefault(){
+
+    // Make the Section visible
+    document.getElementById("user-default").style.display = "block";
+
+    document.getElementById("user-tasks").style.display = "none";
+    document.getElementById("user-reviews").style.display = "none";
+}
+
+function getTasks(){
+    
+    document.getElementById("user-tasks").style.display = "block";
+
+    document.getElementById("user-default").style.display = "none";
+    document.getElementById("user-reviews").style.display = "none";
+}
+
+function getReviews(){
+// function getReviews(all_reviews){
+
+    // Make the Section visible
+    document.getElementById("user-reviews").style.display = "block";
+
+    document.getElementById("user-tasks").style.display = "none";
+    document.getElementById("user-default").style.display = "none";
+}
