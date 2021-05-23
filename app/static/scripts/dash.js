a8a84c63d0a699e8c9ddcc8295079261a31e0969
Shemar Jones <jones.shemar.sj@gmail.com>
2021-05-23 13:28:34 -0500

    Update dash.js

--- app/static/scripts/dash.js
+++ app/static/scripts/dash.js
@@ -1,4 +1,9 @@
 
+// Global Variables
+var default_tab = document.getElementById("default");
+var tasks_tab = document.getElementById("new-task");
+var reviews_tab = document.getElementById("new-review");
+
 // Open & Close of the Aside
 function openNav() {
     document.getElementById("aside-open").style.display = "none";
@@ -16,22 +21,44 @@ function closeNav() {
     // document.getElementById("board").style.marginLeft= "0";
 }
 
-function testTasks(all_tasks){
-    console.log(all_tasks);
+function getDefault(){
+
+    // Make the Section visible
+    document.getElementById("user-default").style.display = "block";
+
+    document.getElementById("user-tasks").style.display = "none";
+    document.getElementById("user-reviews").style.display = "none";
+}
+
+function getTasks(all_tasks){
+    // console.log(all_tasks);
 
     var code = "";
 
     for (const [index, obj] of Object.entries(all_tasks)) {
         console.log(index, obj);
-        var result = "<div class='task'><div class='task-data'><p class='task-title'>" + obj['title'] + "</p><p class='task-body'>" + obj['message'] + "</p></div><div class='task-buttons'><button id='edit'>Edit</button><button id='delete'>Delete</button></div></div>";
+        var result = "<div class='task'><div class='task-data'><p class='task-title'>" + obj['title'] + "</p><p class='task-body'>" + obj['message'] + "</p></div>";
+        var button_section = "<div class='task-buttons'><button id='edit'>Edit</button><button id='delete'>Delete</button></div></div>";
 
-        code += result;
+        code += (result + button_section);
     }
 
-    
-
-    console.log("Code is: " + code);
-
+    // Add the text
     var section = document.getElementById("task-section");
     section.innerHTML = code;
+
+    // Make the Section visible
+    document.getElementById("user-tasks").style.display = "block";
+
+    document.getElementById("user-default").style.display = "none";
+    document.getElementById("user-reviews").style.display = "none";
+}
+
+function getReviews(all_reviews){
+
+    // Make the Section visible
+    document.getElementById("user-reviews").style.display = "block";
+
+    document.getElementById("user-tasks").style.display = "none";
+    document.getElementById("user-default").style.display = "none";
 }
