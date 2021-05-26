34ea847e6fda9ee5ec7a14d26866e95cc18c1a08
Shemar Jones <jones.shemar.sj@gmail.com>
2021-05-26 15:50:35 -0500

    Updated the Team cards
    Updated a few images in the cards on the about us page.

--- app/views.py
+++ app/views.py
@@ -63,7 +63,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html')
+    return render_template('aboutDJS.html')
 
 
 @app.route('/reviews/', methods=['GET', 'POST'])
